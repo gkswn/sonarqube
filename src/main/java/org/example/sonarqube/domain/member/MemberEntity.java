120a8aeb1a47c2d46d4f9e7bd5ce1296a3563023
hanju <ahj1903@naver.com>
2024-05-30 23:51:23 +0900

    create member domain

--- src/main/java/org/example/sonarqube/domain/member/MemberEntity.java
+++ src/main/java/org/example/sonarqube/domain/member/MemberEntity.java
@@ -1,7 +1,6 @@
 package org.example.sonarqube.domain.member;
 
-import javax.persistence.Column;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -11,7 +10,6 @@ public class MemberEntity {
 	@Id
 	@GeneratedValue
 	private Long id;
-
 	@Column
 	private String name;
 
