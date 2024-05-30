c5b4332b33769e60e46f52e8d68e9f8726f2b87e
hanju <ahj1903@naver.com>
2024-05-30 23:20:14 +0900

    first commit

---
+++ src/test/java/org/example/sonarqube/SonarqubeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sonarqube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
